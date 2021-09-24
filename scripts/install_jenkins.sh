0ee29322a6ed1fb40691e24830c0250f89cea366
Swatipat <84763690+Swatipat@users.noreply.github.com>
2021-09-24 14:31:42 -0400

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -4,9 +4,9 @@
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+sudo add-apt-repository 
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu 
+   $(lsb_release -cs) 
    stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
